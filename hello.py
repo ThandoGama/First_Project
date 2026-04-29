37d32ab6deac9f72b05a29b586c472510a4465dc
ThandoGama <thandogama42@gmail.com>
2026-04-29 16:54:28 +0200

    First Python

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Welcome to my learning environment")
+user_name = input("Please enter your name: ")
+user_city = input("Please enter your city: ")
+user = {"name": user_name, "city": user_city}
+
+print(f"Hello {user_name} from {user_city}! Nice to meet you.")
